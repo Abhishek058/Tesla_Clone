78104d438335f56817ad705a2fd37e45e4a628b6
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-28 05:10:25 +0530

    basic files added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import Header from "./Components/Header";
+import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">
+    <Header />
+    <Home />
+  </div>;
 }
 
 export default App;
