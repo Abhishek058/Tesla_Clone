fa26a131609b4c92e65810c69430309f9e0a39dd
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-28 11:12:00 +0530

    section basic code dome

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -8,10 +8,13 @@ function Section() {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
-      <ButtonGroup>
-        <LeftButton>CUSTOM ORDER</LeftButton>
-        <RightButton>EXISTING INVENTORY</RightButton>
-      </ButtonGroup>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>CUSTOM ORDER</LeftButton>
+          <RightButton>EXISTING INVENTORY</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg"></DownArrow>
+      </Buttons>
     </Wrap>
   );
 }
@@ -25,18 +28,44 @@ const Wrap = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ItemText = styled.div`
   padding-top: 15vh;
 `;
 
-const ButtonGroup = styled.div``;
-const LeftButton = styled.div`
-    background-color: rgba(23, 26, 32, 0.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
 `;
-const RightButton = styled(LeftButton)``;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.9;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+const RightButton = styled(LeftButton)`
+  background: white;
+  color: black;
+  opacity: 0.65;
+`;
+const Buttons = styled.div``;
+const DownArrow = styled.img`
+  height: 40px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  overflow-x: hidden;
+  animation: animationDown infinite 2s;
+`;
