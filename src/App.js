6647d1bc1ecf7e98fb92f706eb88571136725132
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-28 09:58:53 +0530

    images added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Counter } from "./features/counter/Counter";
 import Header from "./Components/Header";
 import "./App.css";
 import Home from "./Components/Home";
+import Section from "./Components/Section";
 
 function App() {
   return <div className="App">
     <Header />
     <Home />
+    <Section />
   </div>;
 }
 
